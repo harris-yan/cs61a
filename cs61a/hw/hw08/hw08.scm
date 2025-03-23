dd252d08512d18f4b10f07ae1fef7221ed151d15
harrisyan <harrisyanfromfdu@gmail.com>
2025-03-23 15:49:57 +0800

    Add files via upload

---
+++ cs61a/hw/hw08/hw08.scm
@@ -0,0 +1,25 @@
+(define (ascending? s) 
+    (if (or (null? s) (null? (cdr s)))
+        #t
+        (if (<= (car s) (car (cdr s)))
+            (ascending? (cdr s))
+            #f)))
+
+(define (my-filter pred s)
+    (if (null? s)
+        nil
+        (if (pred (car s))
+            (cons (car s) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s)))))
+
+(define (interleave lst1 lst2)
+    (if (null? lst1)
+        lst2
+        (if (null? lst2)
+            lst1
+            (cons (car lst1) (cons (car lst2) (interleave (cdr lst1) (cdr lst2)))))))
+
+(define (no-repeats s)
+    (if (null? s) 
+        ()
+        (cons (car s) (no-repeats (my-filter (lambda (x) (not (= x (car s))))(cdr s))))))
